a9f1637ee2d44e2b787fe74379fa0a620a8643e0
StuartCowley <voidstalker@live.co.uk>
2023-06-17 22:54:24 +0100

    Added raw form component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "../styles/app.css";
 import initialData from "../data/sampleData.json";
+import RecordForm from "./RecordForm";
 import Record from "./Record";
 
 const App = () => {
@@ -10,7 +11,9 @@ const App = () => {
       <div className="app__background-wrap" />
       <div className="app__foreground-wrap">
         <div className="app__title">Events and forms in React</div>
-        <div className="app__form-wrapper">Form goes here!</div>
+        <div className="app__form-wrapper">
+          <RecordForm />
+        </div>
         <div className="app__records-wrapper">
           {records.map((record) => {
             return <Record record={record} />;
---
+++ src/components/RecordForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecordForm = () => {
+  return (
+    <form className="record-form">
+      <div className="record-form__title">Add New Record Form</div>
+    </form>
+  );
+};
+
+export default RecordForm;
